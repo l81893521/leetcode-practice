77eda187eb93e1fefbe73df7b9ad25a72e3b4a33
will <349071347@qq.com>
2019-11-07 19:10:21 +0800

    fix 344

--- src/main/java/will/zhang/UsingArray/ReverseString344/Solutio2.java
+++ src/main/java/will/zhang/UsingArray/ReverseString344/Solutio2.java
@@ -1,5 +1,6 @@
 package will.zhang.UsingArray.ReverseString344;
 
+
 /**
  * Created by Will.Zhang on 2018/4/9 0009 14:29.
  * Leetcode 344号题目
@@ -9,26 +10,26 @@ package will.zhang.UsingArray.ReverseString344;
  */
 public class Solutio2 {
 
-    public String reverseString(String s) {
-        char[] c = s.toCharArray();
+    public void reverseString(char[] s) {
         /*
         使用对撞指针的思想
         在数组的两侧分别定义i 和 j
         两个指针不断往中间移动, 不断交换字符
          */
-        int i = 0, j = c.length - 1;
+        int i = 0, j = s.length - 1;
 
         while(i <= j){
-            char temp = c[i];
-            c[i] = c[j];
-            c[j] = temp;
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
             i++;
             j--;
         }
-        return new String(c);
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solutio2().reverseString("hello"));
+        char[] c = "hello".toCharArray();
+        new Solutio2().reverseString(c);
+        System.out.println(new String(c));
     }
 }
