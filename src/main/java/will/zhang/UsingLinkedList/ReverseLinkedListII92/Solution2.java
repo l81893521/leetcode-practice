31f56fa038569d10d4985a1fcd7252ed28bc4c10
will.zhang <349071347@qq.com>
2022-02-18 20:00:28 +0800

    add new solution for leetcode no.92

--- src/main/java/will/zhang/UsingLinkedList/ReverseLinkedListII92/Solution2.java
+++ src/main/java/will/zhang/UsingLinkedList/ReverseLinkedListII92/Solution2.java
@@ -10,8 +10,12 @@ public class Solution2 {
 
     private ListNode successor;
 
-    public ListNode reverseBetween(ListNode head, int m, int n) {
-
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (left == 1) {
+            return reverseN(head, right);
+        }
+        head.next = reverseBetween(head.next, left - 1, right - 1);
+        return head;
     }
 
     public ListNode reverseN(ListNode head, int n) {
@@ -21,11 +25,14 @@ public class Solution2 {
         }
 
         ListNode theNthNode = reverseN(head.next, n - 1);
+        head.next.next = head;
+        head.next = successor;
+        return theNthNode;
     }
 
     public static void main(String[] args) {
-        ListNode listNode = ListNode.create(new int[]{1, 2, 3, 4, 5});
-        ListNode result = new Solution2().reverseN(listNode, 3);
+        ListNode listNode = ListNode.create(new int[]{1, 2, 3, 4, 5, 6});
+        ListNode result = new Solution2().reverseBetween(listNode, 3, 5);
         System.out.println(result);
     }
 }
