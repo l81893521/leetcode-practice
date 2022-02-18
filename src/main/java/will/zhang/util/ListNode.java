b2ad24357e254471a56a147b9ac386691962af0a
will.zhang <349071347@qq.com>
2022-02-18 19:57:26 +0800

    optimize the code

--- src/main/java/will/zhang/util/ListNode.java
+++ src/main/java/will/zhang/util/ListNode.java
@@ -8,12 +8,14 @@ public class ListNode {
     public int val;
     public ListNode next;
 
-    public ListNode() {
-
-    }
+    public ListNode() {}
 
     public ListNode(int x) {
         this.val = x;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val; this.next = next;
     }
 
     public static ListNode create(int[] array) {
