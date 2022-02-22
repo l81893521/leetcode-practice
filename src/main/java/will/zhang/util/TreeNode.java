337f57b348fe8ef43cf3838b8f7d0733ac21f188
will.zhang <349071347@qq.com>
2022-02-22 19:55:51 +0800

    add TreeNode Util

---
+++ src/main/java/will/zhang/util/TreeNode.java
@@ -0,0 +1,25 @@
+package will.zhang.util;
+
+/**
+ * @author: jiawei.zhang
+ * @date: 2022/2/22
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {
+
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
