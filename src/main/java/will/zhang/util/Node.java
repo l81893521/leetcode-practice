90f3863844963f5db20a50ef3b73acb09454b6c3
will.zhang <349071347@qq.com>
2022-02-25 20:09:55 +0800

    add node util

---
+++ src/main/java/will/zhang/util/Node.java
@@ -0,0 +1,13 @@
+package will.zhang.util;
+
+/**
+ * @author: jiawei.zhang
+ * @date: 2022/2/25
+ */
+public class Node {
+
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+}
