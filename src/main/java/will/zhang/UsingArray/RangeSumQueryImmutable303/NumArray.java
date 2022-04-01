e1cd81fe01140b8569cbadbb9ba12f4df5af3308
will.zhang <349071347@qq.com>
2022-04-01 17:55:36 +0800

    optimize the code of leetcode.303

--- src/main/java/will/zhang/UsingArray/RangeSumQueryImmutable303/NumArray.java
+++ src/main/java/will/zhang/UsingArray/RangeSumQueryImmutable303/NumArray.java
@@ -1,20 +1,5 @@
 package will.zhang.UsingArray.RangeSumQueryImmutable303;
 
-/**
- * LeetCode 303. 区域和检索 - 数组不可变
- * 给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。
- * 示例：
- * 给定 nums = [-2, 0, 3, -5, 2, -1]，求和函数为sumRange()
- *
- * sumRange(0, 2) -> 1
- * sumRange(2, 5) -> -1
- * sumRange(0, 5) -> -3
- *
- * 说明:
- *
- * 你可以假设数组不可变。
- * 会多次调用 sumRange 方法。
- */
 public class NumArray {
 
     //sum[i] = nums[0...i-1]的和
@@ -28,8 +13,8 @@ public class NumArray {
         }
     }
 
-    public int sumRange(int i, int j) {
-        return sum[j + 1] - sum[i];
+    public int sumRange(int left, int right) {
+        return sum[right + 1] - sum[left];
     }
 
     public static void main(String[] args) {
@@ -37,9 +22,3 @@ public class NumArray {
         System.out.println(numArray.sumRange(0, 2));
     }
 }
-
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray obj = new NumArray(nums);
- * int param_1 = obj.sumRange(i,j);
- */
